56509ee932979c86f8cf71c8d484a84c83373363
John Gasper <jtgasper3@gmail.com>
2017-01-12 09:59:13 -0800

    Adding Docker testbed (minus loader.properties) which has sensitive info

---
+++ docker/test.sh
@@ -0,0 +1,12 @@
+docker-compose rm -f
+rm -r ./grouper/temp/
+mkdir -p ./grouper/temp/
+
+pushd ..
+mvn clean package dependency:copy-dependencies -DskipTests -DincludeScope=runtime
+
+popd
+cp -r ../target/lib ./grouper/temp/
+cp ../target/google-*.jar ./grouper/temp/lib/
+
+docker-compose build
